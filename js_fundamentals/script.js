ae3ad2b8e4e87103f1dba27ae6a4d3a765352203
Fahinur Haque <lina-22@SuperMac.local>
2021-04-25 14:19:26 +0200

    my works last wekk april

---
+++ js_fundamentals/script.js
@@ -0,0 +1,79 @@
+// learning variables -
+// let , const
+
+// const name = "lina";
+// let age = 300
+// let x = null;
+// let manush = {
+//     hat : "2 ta",
+//     pao : "4ta",
+//     nak : 10, 
+//     chul: 'black'
+// };
+// manush.chuk = "2ta"
+// let hat = manush.hat;
+// let pao = manush.pao;
+// let {hat, pao} = manush;
+// hat = "3ta";
+// console.log(manush);
+// let y = true;
+// let z = undefined;
+// name = "kashif"
+
+// console.log(name);
+
+// let score = 0;
+// score = 10;
+// score = 20;
+
+// console.log(score);
+
+// let name = "Sayed Kashif";
+// let upperCasedName = name.toUpperCase();
+// let lowerCase = name.toLowerCase();
+// let fName = name.split(' ')[3];
+// let nameList = ['Sayed', 'Kashif'];
+
+// console.log(fName);
+
+
+
+// ******* working with array of objects ********* -
+
+// let tomorrowWorkList = [
+//     {
+//         no : 1,
+//         kam : "ghum brush kora",
+//         isDone : true,
+//         address : {
+//             houseNo : 6,
+//             road : "uiuiu"
+//         }
+//     },
+//     {
+//         no : 2,
+//         kam : "nasta gorTamani kaisanGusol",
+//         isDone : false
+//     },
+
+//     {
+//         no : 3,
+//         kam : "randa meman kawa",
+//         isDone : true
+//     }
+// ]
+
+// let tomorrowWorkListJSON = JSON.stringify(tomorrowWorkList);
+// let bokkorChokkor = JSON.parse(tomorrowWorkListJSON);
+// console.log(bokkorChokkor);
+
+// data format - used to simplify data and communicate easily.
+// JSON = JAVASCRIPT OBJECT Notation.
+
+// for loop -
+
+for(let i = 0; i < 99; i += 5 ){
+    console.log(i);
+}
+
+
